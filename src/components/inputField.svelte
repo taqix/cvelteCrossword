008b14a9d039fee18aee252371213c671e4f73ae
taqix <taqiedits@gmail.com>
2022-12-06 13:48:49 +0100

    static commit for general structure

---
+++ src/components/inputField.svelte
@@ -0,0 +1,15 @@
+<script>
+export let validValue
+</script>
+
+<input type="text" maxlength="1">
+
+<style>
+    input{
+        width:10%;
+        height: 50px;
+        border: 1px solid green;
+        text-align: center;
+        text-transform: uppercase;
+    }
+</style>
---
+++ src/components/rowFields.svelte
@@ -0,0 +1,10 @@
+<script>
+import InputField from "./inputField.svelte";
+export let xSize;
+</script>
+{#each Array(xSize) as _}
+    <InputField />
+{/each}
+<style>
+
+</style>
