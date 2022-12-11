0ede877981c73432658b394ab9572f18259063a5
Tomasz <taqiedits@gmail.com>
2022-12-11 14:16:15 +0100

    fixed some bugs and add working going by arrows

--- src/components/rowFields.svelte
+++ src/components/rowFields.svelte
@@ -31,11 +31,43 @@
   }
 
   function changingFocus(e) {
+    checkIfLettersValid(e.target.value, e.target.id);
     const oldId = parseInt(e.target.id);
     console.log(e.keyCode);
-    if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 38 || e.keyCode === 40 || e.keyCode === 18) {
+    console.log(wordTrue);
+    if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode == 39) {
+      if(e.target.id.slice(0,1)==4 && e.target.id.slice(-1) == 4){
+        return;
+      }
+      if ((e.keyCode === 13 && e.target.id.slice(-1) == 4) || wordTrue) {
+        const startId = parseInt(e.target.id.slice(0, 1)) + 1;
+        document.getElementById(startId + "0").focus();
+        return;
+      }
+      
+      if (e.target.id.slice(-1) == 4) {
+        const startId = e.target.id.slice(0, 1);
+        document.getElementById(startId + "0").focus();
+        return;
+      }
+      const newId = getNewId(oldId, true);
+      if (e.target.id.slice(0, 1) == 0) {
+        document.getElementById("0" + newId).focus();
+      } else {
+        document.getElementById(newId).focus();
+      }
       return;
     }
+    if (
+      e.keyCode === 8 ||
+      e.keyCode === 46 ||
+      e.keyCode === 38 ||
+      e.keyCode === 40 ||
+      e.keyCode === 18
+    ) {
+      return;
+    }
+
     if (e.keyCode === 37) {
       const newId = getNewId(oldId, false);
       if (e.target.id.slice(0, 1) == 0) {
@@ -45,14 +77,6 @@
       }
       return;
     }
-
-    const newId = getNewId(oldId, true);
-    if (e.target.id.slice(0, 1) == 0) {
-      document.getElementById("0" + newId).focus();
-    } else {
-      document.getElementById(newId).focus();
-    }
-    return
   }
 </script>
 
@@ -61,7 +85,7 @@
     <input
       type="text"
       maxlength="1"
-      on:change={(e) => checkIfLettersValid(e.target.value, e.target.id)}
+      disabled={wordTrue}
       id={indexOfRow + String(index)}
       class={wordTrue ? "greened" : ""}
       on:keyup|preventDefault={(e) => changingFocus(e)}
